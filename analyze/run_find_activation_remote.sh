427bf1419a1fd9743d69dafa6509781750f34489
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-03 03:45:46 +0800

    Update running script

--- analyze/run_find_activation_remote.sh
+++ analyze/run_find_activation_remote.sh
@@ -1,5 +1,5 @@
 mkdir -p output
-python analyze/find_activation.py --gpu --num 100 \
+python find_activation.py --gpu --num 100 \
     ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
     /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list activation_out conv3,conv4,conv5 \
     </dev/null >output/activation.out 2>output/activation.err &
