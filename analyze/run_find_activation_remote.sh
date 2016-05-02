0677a5a505e07587a48013c8b430e3aec34f81aa
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-02 22:39:44 +0800

    Correct two bugs with script

--- analyze/run_find_activation_remote.sh
+++ analyze/run_find_activation_remote.sh
@@ -1,6 +1,6 @@
-mkdir output
+mkdir -p output
 cd ..
 python analyze/find_activation.py --gpu --num 100 \
     models/caffenet-yos/caffenet-yos-deploy.prototxt models/caffenet-yos/caffenet-yos-weights \
     /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list activation_out conv3,conv4,conv5 \
-    </dev/null >analyze/output/activation.out 2>analyze/output/activation.err
+    </dev/null >analyze/output/activation.out 2>analyze/output/activation.err &
