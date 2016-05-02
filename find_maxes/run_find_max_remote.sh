a67d6cfa78e1f94348263c8960111912fa77592c
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-03 03:28:24 +0800

    Update script to compute maximum activations for each unit

--- find_maxes/run_find_max_remote.sh
+++ find_maxes/run_find_max_remote.sh
@@ -1,1 +1,5 @@
-python find_maxes/find_max_acts.py --N 9 --gpu models/caffenet-yos/caffenet-yos-deploy.prototxt models/caffenet-yos/caffenet-yos-weights /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list max_out >stdout.out 2>stderr.err </dev/null &
+mkdir -p output
+python find_max_acts.py --N 9 --gpu \
+    ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
+    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list output/max_out \
+    >output/find_max.out 2>output/find_max.err </dev/null &
