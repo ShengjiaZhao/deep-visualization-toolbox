f4ba7373835c6077dafc159e4e7ffc13356b6bd4
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-03 05:04:59 +0800

    Prepare to run remotely

--- analyze/run_find_activation_remote.sh
+++ analyze/run_find_activation_remote.sh
@@ -1,5 +1,5 @@
 mkdir -p output
-python find_activation.py --gpu --num 100 \
+python find_activation.py --gpu --num 200000 \
     ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
     /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list activation_out conv3,conv4,conv5 \
     </dev/null >output/activation.out 2>output/activation.err &
