50ed92bcec4739bee0aafd5f1d66fe0c6c0a6078
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-03 10:23:34 +0800

    Compute max patch for fc layers

--- find_maxes/run_crop_max_patches_remote.sh
+++ find_maxes/run_crop_max_patches_remote.sh
@@ -1,29 +1,14 @@
 python crop_max_patches.py --N 9 --gpu \
-    --do-maxes --do-deconv --do-deconv-norm --do-backprop --do-backprop-norm --do-info \
-    ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
-    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results conv4
-
-python crop_max_patches.py --N 9 --gpu \
-    --do-maxes --do-deconv --do-deconv-norm --do-backprop --do-backprop-norm --do-info \
-    ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
-    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results conv3
-
-python crop_max_patches.py --N 9 --gpu \
-    --do-maxes --do-deconv --do-deconv-norm --do-backprop --do-backprop-norm --do-info \
-    ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
-    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results conv2
-
-python crop_max_patches.py --N 9 --gpu \
-    --do-maxes --do-deconv --do-deconv-norm --do-backprop --do-backprop-norm --do-info \
+    --do-maxes --do-deconv --do-deconv-norm --do-info \
     ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
     /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results fc6
 
 python crop_max_patches.py --N 9 --gpu \
-    --do-maxes --do-deconv --do-deconv-norm --do-backprop --do-backprop-norm --do-info \
+    --do-maxes --do-deconv --do-deconv-norm --do-info \
     ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
     /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results fc7
 
 python crop_max_patches.py --N 9 --gpu \
-    --do-maxes --do-deconv --do-deconv-norm --do-backprop --do-backprop-norm --do-info \
+    --do-maxes --do-deconv --do-deconv-norm --do-info \
     ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
     /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results fc8
