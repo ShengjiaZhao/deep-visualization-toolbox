55c61b637c744f1cd140a436994f876aed725c38
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-03 10:04:48 +0800

    Script to run all layers

---
+++ find_maxes/run_crop_max_patches_all.sh
@@ -0,0 +1,2 @@
+mkdir -p output
+./run_crop_max_patches_remote.sh >output/run_crop_max.out 2>output/run_crop_max.err </dev/null &
--- find_maxes/run_crop_max_patches_remote.sh
+++ find_maxes/run_crop_max_patches_remote.sh
@@ -1,8 +1,29 @@
-mkdir -p output
+python crop_max_patches.py --N 9 --gpu \
+    --do-maxes --do-deconv --do-deconv-norm --do-backprop --do-backprop-norm --do-info \
+    ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
+    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results conv4
 
 python crop_max_patches.py --N 9 --gpu \
     --do-maxes --do-deconv --do-deconv-norm --do-backprop --do-backprop-norm --do-info \
     ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
-    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results conv5 \
-    </dev/null >output/crop_max_patches.out 2>output/crop_max_patches.err &
+    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results conv3
 
+python crop_max_patches.py --N 9 --gpu \
+    --do-maxes --do-deconv --do-deconv-norm --do-backprop --do-backprop-norm --do-info \
+    ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
+    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results conv2
+
+python crop_max_patches.py --N 9 --gpu \
+    --do-maxes --do-deconv --do-deconv-norm --do-backprop --do-backprop-norm --do-info \
+    ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
+    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results fc6
+
+python crop_max_patches.py --N 9 --gpu \
+    --do-maxes --do-deconv --do-deconv-norm --do-backprop --do-backprop-norm --do-info \
+    ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
+    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results fc7
+
+python crop_max_patches.py --N 9 --gpu \
+    --do-maxes --do-deconv --do-deconv-norm --do-backprop --do-backprop-norm --do-info \
+    ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
+    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results fc8
