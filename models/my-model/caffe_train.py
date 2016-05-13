e207d35e210c09cad5ca6f9fd373fba91eac7fe5
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-13 12:39:59 +0800

    Script to train caffe models

---
+++ models/my-model/caffe_train.py
@@ -0,0 +1,63 @@
+__author__ = 'shengjia'
+
+import sys
+import numpy as np
+
+# The caffe module needs to be on the Python path;
+#  we'll add it here explicitly.
+caffe_root = '/home/ubuntu/caffe/'  # this file should be run from {caffe_root}/examples (otherwise change this line)
+sys.path.insert(0, caffe_root + 'python')
+import caffe
+caffe.set_device(0)
+caffe.set_mode_gpu()
+
+import os
+os.chdir('/home/ubuntu/deep-visualization-toolbox/models/my-model')
+
+### load the solver and create train and test nets
+solver = None  # ignore this workaround for lmdb data (can't instantiate two solvers on the same data)
+solver = caffe.SGDSolver('original_solver.prototxt')
+
+transformer = caffe.io.Transformer({'data': solver.net.blobs['data'].data.shape})
+data_mean = np.load('ilsvrc12/ilsvrc_2012_mean.npy').mean(1).mean(1)
+transformer.set_transpose('data', (2,0,1))
+transformer.set_mean('data', data_mean - 8) # Subtract a bit more to avoid overflow，　only for visualizatio
+transformer.set_channel_swap('data', (2,1,0))
+transformer.set_raw_scale('data', 255.0)
+
+lookup_file = 'ilsvrc12/synset_words.txt'
+lines = open(lookup_file).readlines()
+names = [line.split()[1] for line in lines]
+
+import time
+niter = 20000
+test_interval = 100
+# losses will also be stored in the log
+train_loss = np.zeros(niter)
+test_acc = np.zeros(int(np.ceil(niter / test_interval)))
+output = np.zeros((niter, 8, 10))
+
+begin_time = time.time()
+# the main solver loop
+for it in range(niter):
+    step_time = time.time()
+    solver.step(1)  # SGD by Caffe
+    elapsed_time = time.time() - step_time
+
+    # store the train loss
+    train_loss[it] = solver.net.blobs['loss'].data
+    print("Iter " + str(it) + " -- Train loss: " + str(train_loss[it]) +
+          " -- Time used: " + str(elapsed_time) + "s -- Total time: " + str(time.time() - begin_time) + "s")
+
+    # run a full test every so often
+    # (Caffe can also do this for us and write to a log, but we show here
+    #  how to do it directly in Python, where more complicated things are easier.)
+    if it % test_interval == 0:
+        print 'Iteration', it, 'testing...'
+        correct = 0
+        for test_it in range(100):
+            solver.test_nets[0].forward()
+            correct += sum(solver.test_nets[0].blobs['fc8'].data.argmax(1)
+                           == solver.test_nets[0].blobs['label'].data)
+        test_acc[it // test_interval] = correct / 1e4
+        print("Test accuracy: " + str(correct / 1e4))
