dfd4e9f214bfae0e6c21cd30a97803d5816b6560
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-14 22:06:02 +0800

    Change script to run a different network

--- find_maxes/run_find_max_remote.sh
+++ find_maxes/run_find_max_remote.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 mkdir -p output
+#python find_max_acts.py --N 9 --gpu \
+#    ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
+#    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list output/max_out \
+#    >output/find_max.out 2>output/find_max.err </dev/null &
+
+
+
 python find_max_acts.py --N 9 --gpu \
-    ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
-    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list output/max_out \
+    ../models/my-model/original_deploy.prototxt ../models/my_model/output/sparse_solver.caffemodel \
+    /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list output/max_out_sparse \
     >output/find_max.out 2>output/find_max.err </dev/null &
