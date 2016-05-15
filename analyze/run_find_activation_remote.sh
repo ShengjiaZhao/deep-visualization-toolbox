decdf7754c4911d0cbf1c02f2eba5d62aa7bf70d
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-15 21:49:30 +0800

    Script to run remotely

--- analyze/run_find_activation_remote.sh
+++ analyze/run_find_activation_remote.sh
@@ -1,5 +1,14 @@
+#!/usr/bin/env bash
 mkdir -p output
+mkdir -p /home/ubuntu/sdg/activations
+
+# python find_activation.py --gpu --num 200000 \
+#     ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
+#     /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list activation_out conv3,conv4,conv5,fc6,fc7 \
+#     </dev/null >output/activation.out 2>output/activation.err &
+
+
 python find_activation.py --gpu --num 200000 \
-    ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
-    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list activation_out conv3,conv4,conv5,fc6,fc7 \
+    ../models/my-model/original_deploy.prototxt ../models/my-model/output/sparse_solver.caffemodel \
+    /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list /home/ubuntu/sdg/activations conv3,conv4,conv5,fc6,fc7 \
     </dev/null >output/activation.out 2>output/activation.err &
