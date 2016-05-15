c855310c86fa79a45a402d2e1ac73fd05477addd
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-15 21:23:33 +0800

    Run new network

--- find_maxes/run_crop_max_patches_remote.sh
+++ find_maxes/run_crop_max_patches_remote.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-python crop_max_patches.py --N 9 --gpu \
-    --do-maxes --do-deconv --do-deconv-norm --do-info \
-    ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
-    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results fc6
+mkdir -p /home/ubuntu/sdg/results
 
 python crop_max_patches.py --N 9 --gpu \
     --do-maxes --do-deconv --do-deconv-norm --do-info \
-    ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
-    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results fc7
+    output/max_out_sparse ../models/my-model/original_deploy.prototxt ../models/my-model/output/sparse_solver.caffemodel \
+    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdg/results conv3
 
 python crop_max_patches.py --N 9 --gpu \
     --do-maxes --do-deconv --do-deconv-norm --do-info \
-    ../max_out ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
-    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdf/results fc8
+    output/max_out_sparse ../models/my-model/original_deploy.prototxt ../models/my-model/output/sparse_solver.caffemodel \
+    /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list  /home/ubuntu/sdg/results conv4
+
+python crop_max_patches.py --N 9 --gpu \
+    --do-maxes --do-deconv --do-deconv-norm --do-info \
+    output/max_out_sparse ../models/my-model/original_deploy.prototxt ../models/my-model/output/sparse_solver.caffemodels \
+    /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list  /home/ubuntu/sdg/results conv5
