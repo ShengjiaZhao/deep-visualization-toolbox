70f338d749e2ca95af3a07f8f9e62fcb1a5431d7
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-15 21:42:01 +0800

    Run conv5 and conv3 layers

--- find_maxes/run_crop_max_patches_remote.sh
+++ find_maxes/run_crop_max_patches_remote.sh
@@ -4,14 +4,15 @@ mkdir -p /home/ubuntu/sdg/results
 python crop_max_patches.py --N 9 --gpu \
     --do-maxes --do-deconv --do-deconv-norm --do-info \
     output/max_out_sparse ../models/my-model/original_deploy.prototxt ../models/my-model/output/sparse_solver.caffemodel \
-    /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list /home/ubuntu/sdg/results conv3
+    /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list  /home/ubuntu/sdg/results conv5
 
 python crop_max_patches.py --N 9 --gpu \
     --do-maxes --do-deconv --do-deconv-norm --do-info \
     output/max_out_sparse ../models/my-model/original_deploy.prototxt ../models/my-model/output/sparse_solver.caffemodel \
-    /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list  /home/ubuntu/sdg/results conv4
+    /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list /home/ubuntu/sdg/results conv3
 
-python crop_max_patches.py --N 9 --gpu \
-    --do-maxes --do-deconv --do-deconv-norm --do-info \
-    output/max_out_sparse ../models/my-model/original_deploy.prototxt ../models/my-model/output/sparse_solver.caffemodels \
-    /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list  /home/ubuntu/sdg/results conv5
+#python crop_max_patches.py --N 9 --gpu \
+#    --do-maxes --do-deconv --do-deconv-norm --do-info \
+#    output/max_out_sparse ../models/my-model/original_deploy.prototxt ../models/my-model/output/sparse_solver.caffemodel \
+#g    /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list  /home/ubuntu/sdg/results conv4
+
