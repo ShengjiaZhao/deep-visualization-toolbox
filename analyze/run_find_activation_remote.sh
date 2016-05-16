0620e06d41caaa78c87429504331b7160de59ed6
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-16 13:19:25 +0800

    Run more images

--- analyze/run_find_activation_remote.sh
+++ analyze/run_find_activation_remote.sh
@@ -5,10 +5,10 @@ mkdir -p /home/ubuntu/sdg/activations
 # python find_activation.py --gpu --num 200000 \
 #     ../models/caffenet-yos/caffenet-yos-deploy.prototxt ../models/caffenet-yos/caffenet-yos-weights \
 #     /home/ubuntu/sdf/images /home/ubuntu/sdf/database_list activation_out conv3,conv4,conv5,fc6,fc7 \
-#     </dev/null >output/activation.out 2>output/activation.err &
+#     </dev/null >output/stdout 2>output/stderr &
 
 
-python find_activation.py --gpu --num 5000 \
+python find_activation.py --gpu --num 80000 \
     ../models/my-model/original_deploy.prototxt ../models/my-model/output/sparse_solver.caffemodel \
     /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list /home/ubuntu/sdg/activations conv3,conv4,conv5,fc6,fc7 \
-    </dev/null >output/activation.out 2>output/activation.err &
+    </dev/null >output/stdout 2>output/stderr &
