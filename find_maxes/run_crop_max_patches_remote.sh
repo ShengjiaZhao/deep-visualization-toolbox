fa599781e5061cc15755875b870ba7c300b5b100
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-16 17:09:33 +0800

    Run for all layers

--- find_maxes/run_crop_max_patches_remote.sh
+++ find_maxes/run_crop_max_patches_remote.sh
@@ -11,8 +11,8 @@ python crop_max_patches.py --N 9 --gpu \
     output/max_out_sparse ../models/my-model/original_deploy.prototxt ../models/my-model/output/sparse_solver.caffemodel \
     /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list /home/ubuntu/sdg/results conv3
 
-#python crop_max_patches.py --N 9 --gpu \
-#    --do-maxes --do-deconv --do-deconv-norm --do-info \
-#    output/max_out_sparse ../models/my-model/original_deploy.prototxt ../models/my-model/output/sparse_solver.caffemodel \
-#g    /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list  /home/ubuntu/sdg/results conv4
+python crop_max_patches.py --N 9 --gpu \
+    --do-maxes --do-deconv --do-deconv-norm --do-info \
+    output/max_out_sparse ../models/my-model/original_deploy.prototxt ../models/my-model/output/sparse_solver.caffemodel \
+    /home/ubuntu/sdg/train /home/ubuntu/sdg/image_list  /home/ubuntu/sdg/results conv4
 
