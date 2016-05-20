ca9885114a7bdcc83fa7feb0212bcbbeb408e016
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-05-20 11:20:32 +0800

    Local frontend

--- analyze/image_clustering.py
+++ analyze/image_clustering.py
@@ -78,22 +78,6 @@ class ImageClustering:
         np.save(output_path + 'linkage', Z)
         print("Computed linkage")
 
-        # Plot dendrogram
-        fig = plt.figure(figsize=(25, 10))
-        # self.ax = fig.add_subplot(1, 1, 1)
-        # cid = fig.canvas.mpl_connect('button_press_event', self.onclick)
-        plt.title('Hierarchical Clustering Dendrogram')
-        plt.xlabel('sample index')
-        plt.ylabel('distance')
-        self.ddata = dendrogram(
-            Z,
-            leaf_rotation=90.,  # rotates the x axis labels
-            leaf_font_size=8.,  # font size for the x axis labels
-            truncate_mode='lastp',  # show only the last p merged clusters
-            p=200,  # show only the last p merged clusters
-        )
-        plt.show()
-
         # Create visualizations for each large cluster
         for index in range(1, 100):
             images_in_cluster = []
@@ -105,30 +89,16 @@ class ImageClustering:
                 else:
                     index_stack.append(int(round(Z[cur_index - data_count, 0])))
                     index_stack.append(int(round(Z[cur_index - data_count, 1])))
-            print(len(images_in_cluster))
+
             # Sample images if there are less
             if len(images_in_cluster) > 100:
                 images_in_cluster = random.sample(images_in_cluster, 100)
             images = [{'path': self.image_list[image_index], 'coord': [random.random(), random.random()]}
                       for image_index in images_in_cluster]
-            fig = plt.figure(figsize=(25, 10))
+            # fig = plt.figure(figsize=(25, 10))
             img = self.visualizer.visualize_collage_image(images)
             np.save(output_path + 'cluster' + str(index), img)
             print("Processed img " + str(index))
-            #plt.show()
-            ''' Plot and verify correctness
-            point_list_x = [data[index, 0] for index in images_in_cluster]
-            point_list_y = [data[index, 1] for index in images_in_cluster]
-            plt.xlim([-1, 2])
-            plt.ylim([-1, 2])
-            plt.scatter(point_list_x, point_list_y)
-            plt.show() '''
-            #print(images_in_cluster)
-            #
-
-        self.x_range = np.max(self.ddata['icoord']) - np.min(self.ddata['icoord'])
-        self.y_range = np.max(self.ddata['dcoord']) - np.min(self.ddata['dcoord'])
-
 
     def plot(self, data):
         plt.scatter(data[:,0], data[:,1])
@@ -136,39 +106,73 @@ class ImageClustering:
 
 
 class ImageClusterDisplay:
+    def __init__(self):
+        self.ax = None
+        self.ddata = None
+        self.x_range = None
+        self.y_range = None
+        self.mapping = None
+
     def display(self):
-        Z = np.load(output_path + 'linkage.npz')
+        self.linkage = np.load(output_path + 'linkage.npy')
         # Plot dendrogram
-        fig = plt.figure(figsize=(25, 10))
-        # self.ax = fig.add_subplot(1, 1, 1)
-        # cid = fig.canvas.mpl_connect('button_press_event', self.onclick)
+        fig = plt.figure()
+        self.ax = fig.add_subplot(1, 1, 1)
+        fig.canvas.mpl_connect('button_press_event', self.onclick)
         plt.title('Hierarchical Clustering Dendrogram')
         plt.xlabel('sample index')
         plt.ylabel('distance')
+        dendro_p = 100
         self.ddata = dendrogram(
-            Z,
+            self.linkage,
             leaf_rotation=90.,  # rotates the x axis labels
             leaf_font_size=8.,  # font size for the x axis labels
             truncate_mode='lastp',  # show only the last p merged clusters
-            p=200,  # show only the last p merged clusters
+            p=dendro_p,  # show only the last p merged clusters
+            show_leaf_counts=True,
         )
+        ordering = np.argsort([d[1] for d in self.ddata['dcoord']])[::-1]
+        # Map each item in linkage to its cluster index
+        self.mapping = np.ndarray(dendro_p-1, np.int)
+        for index in range(dendro_p - 1):
+            self.mapping[ordering[index]] = index
+
+        for i, d, c, index in zip(self.ddata['icoord'], self.ddata['dcoord'], self.ddata['color_list'], range(dendro_p-1)):
+            x = 0.5 * sum(i[1:3])
+            y = d[1]
+            if y > 2:
+                plt.plot(x, y, 'o', c=c)
+                plt.annotate(str(self.mapping[index]), (x, y), xytext=(0, -5),
+                             textcoords='offset points',
+                             va='top', ha='center')
+
+        self.x_range = np.max(self.ddata['icoord']) - np.min(self.ddata['icoord'])
+        self.y_range = np.max(self.ddata['dcoord']) - np.min(self.ddata['dcoord'])
+        mng = plt.get_current_fig_manager()
+        mng.resize(*mng.window.maxsize())
         plt.show()
 
     # Event Handler
     def onclick(self, event):
         print('button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
               (event.button, event.x, event.y, event.xdata, event.ydata))
-        for i, d in zip(self.ddata['icoord'], self.ddata['dcoord']):
+        for i, d, index in zip(self.ddata['icoord'], self.ddata['dcoord'], xrange(self.linkage.shape[0])):
             x = 0.5 * sum(i[1:3])
             y = d[1]
             if abs(event.xdata - x) / self.x_range < 0.01 and abs(event.ydata - y) / self.y_range < 0.01:
-                circ = Ellipse(xy=(x, y), width=10, height=0.1)
-                self.ax.add_artist(circ)
-                plt.draw()
-                print("Hit: " + str(x) + " " + str(y))
+                # circ = Ellipse(xy=(x, y), width=10, height=0.1)
+                # self.ax.add_artist(circ)
+                # plt.draw()
+                img = np.load(output_path + 'cluster' + str(self.mapping[index]+1) + '.npy')
+                plt.figure(figsize=(20, 20))
+                plt.imshow(img)
+                plt.title('Cluster ' + str(self.mapping[index]))
+                plt.show()
                 break
 
 if __name__ == '__main__':
-    clusterer = ImageClustering()
-    x = clusterer.generate_synthetic()
-    clusterer.cluster(x)
+    # clusterer = ImageClustering()
+    # x = clusterer.generate_synthetic()
+    #clusterer.cluster(x)
+    displayer = ImageClusterDisplay()
+    displayer.display()
