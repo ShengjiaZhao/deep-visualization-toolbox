70a772b74783f9e16c57c109e1f29a99ac0d2765
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-06-05 21:37:06 +0800

    Change run script

--- models/my-model/run_sparse_train.sh
+++ models/my-model/run_sparse_train.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p output
-python caffe_train.py sparse_solver.prototxt >output/stdout 2>output/stderr </dev/null &
+python caffe_train_sparse.py sparse_solver.prototxt >output/stdout 2>output/stderr </dev/null &
