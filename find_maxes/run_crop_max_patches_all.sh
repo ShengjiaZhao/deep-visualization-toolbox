fa81c3951b81f52e89de2eef5d92813204159f22
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-06-08 11:33:26 +0800

    Script to find max patch for sigmoid network

--- find_maxes/run_crop_max_patches_all.sh
+++ find_maxes/run_crop_max_patches_all.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mkdir -p output
-./run_crop_max_patches_remote.sh >output/run_crop_max.out 2>output/run_crop_max.err </dev/null &
+./run_crop_max_patches_sigmoid.sh >output/run_crop_max.out 2>output/run_crop_max.err </dev/null &
