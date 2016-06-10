1a0b108f91f895a3a474868f0d2ee76868e6fac9
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-06-10 11:40:08 +0800

    Minor changes

--- models/my-model/predictive_network.py
+++ models/my-model/predictive_network.py
@@ -64,42 +64,37 @@ transform_dim = 2
 transform_var = state_variable([batch_size, transform_dim], name='transform_var')
 with tf.name_scope("fc1"):
     fc1_input = tf.concat(1, [tf.select(train_phase, y_ref, y_pred), transform_var])
-    W_fc1 = weight_variable([10+transform_dim, 256])
-    b_fc1 = bias_variable([256])
-    fc1_var = state_variable([batch_size, 256], name='fc1_var')
+    W_fc1 = weight_variable([10+transform_dim, 7*7*32])
+    b_fc1 = bias_variable([7*7*32])
+    fc1_var = state_variable([batch_size, 7*7*32], name='fc1_var')
 
     fc1_relu = tf.nn.relu(tf.matmul(fc1_input, W_fc1, name='fc1') + b_fc1, name='fc1_relu')
-    fc1_loss = tf.reduce_sum(tf.square(tf.sub(fc1_var, fc1_relu)), name='fc1_loss') / (256*batch_size)
-    fc1_reg = -tf.log(tf.reduce_sum(fc1_relu) / (256*batch_size) + 1)
-    fc1_weight_loss = tf.reduce_sum(tf.square(W_fc1)) / (10*256+transform_dim*256)
+    fc1_loss = tf.reduce_sum(tf.square(tf.sub(fc1_var, fc1_relu)), name='fc1_loss') / (7*7*32*batch_size)
+    fc1_reg = -tf.log(tf.reduce_sum(fc1_relu) / (7*7*32*batch_size) + 1)
+    fc1_weight_loss = tf.reduce_sum(tf.square(W_fc1)) / (10*7*7*32+transform_dim*7*7*32)
 
-with tf.name_scope("fc2"):
-    W_fc2 = weight_variable([256, 256])
-    b_fc2 = bias_variable([256])
-    fc2_var = state_variable([batch_size, 256], name='fc2_var')
+with tf.name_scope('conv1'):
+    conv1_in = unpool(tf.reshape(fc1_var, [batch_size, 7, 7, 32]), name='conv1_unpool')
+    W_conv1 = weight_variable([5, 5, 32, 24])
+    b_conv1 = bias_variable([24])
+    conv1_var = state_variable([batch_size, 14, 14, 24], name='conv1_var')
 
-    fc2_relu = tf.nn.relu(tf.matmul(fc1_var, W_fc2, name='fc2') + b_fc2, name='fc2_relu')
-    fc2_loss = tf.reduce_sum(tf.square(tf.sub(fc2_var, fc2_relu)), name='fc2_loss') / (256*batch_size)
-    fc2_reg = -tf.log(tf.reduce_sum(fc2_relu) / (256*batch_size) + 1)
-    fc2_weight_loss = tf.reduce_sum(tf.square(W_fc2)) / (256*256)
+    conv1_relu = tf.nn.relu(tf.nn.conv2d(conv1_in, W_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
+    conv1_loss = tf.reduce_sum(tf.square(tf.sub(conv1_var, conv1_relu)), name='conv1_loss') / (14*14*24*batch_size)
 
-with tf.name_scope("fc3"):
-    W_fc3 = weight_variable([256, 256])
-    b_fc3 = bias_variable([256])
-    fc3_var = state_variable([batch_size, 256], name='fc3_var')
+with tf.name_scope('conv2'):
+    conv2_in = unpool(conv1_var, name='conv2_unpool')
+    W_conv2 = weight_variable([5, 5, 24, 1])
+    b_conv2 = bias_variable([1])
 
-    fc3_relu = tf.nn.relu(tf.matmul(fc2_var, W_fc3, name='fc3') + b_fc3, name='fc3_relu')
-    fc3_loss = tf.reduce_sum(tf.square(tf.sub(fc3_var, fc3_relu)), name='fc3_loss') / (256*batch_size)
-    fc3_weight_loss = tf.reduce_sum(tf.square(W_fc3)) / (256*256)
+    conv2_relu = tf.nn.sigmoid(tf.nn.conv2d(conv2_in, W_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
+    conv2_loss = tf.reduce_sum(tf.square(tf.sub(x_image, conv2_relu)), name='conv2_loss') / (28*28*batch_size)
 
-with tf.name_scope("fc4"):
-    W_fc4 = weight_variable([256, 28 * 28])
-    b_fc4 = bias_variable([28 * 28])
-
-    fc4_relu = tf.reshape(tf.nn.relu(tf.matmul(fc3_var, W_fc4, name='fc4') + b_fc4, name='fc4_relu'), [batch_size] + [28, 28, 1])
-    fc4_loss = tf.reduce_sum(tf.square(tf.sub(x_image, fc4_relu)), name='fc4_loss') / (28*28*batch_size)
-    fc4_weight_loss = tf.reduce_sum(tf.square(W_fc4)) / (256*28*28)
-
+# Some hints:
+# 1. be very careful about the setting of loss coeff. Upper layers should set high loss
+# 2. When the internal nodes become Gaussians with very small variance, does this mean that the network is almost exact
+# but still convex?
+# TODO: Plot posterior activation for internal nodes
 '''
 with tf.name_scope("fc1"):
     fc1_input = tf.select(train_phase, y_ref, y_weight_var)
@@ -138,14 +133,16 @@ with tf.name_scope('conv2'):
     conv2_loss = tf.reduce_sum(tf.square(tf.sub(x_image, conv2_relu)), name='conv2_loss') / (28*28*batch_size)
 '''
 
-total_train_loss = fc1_loss * 20 + fc2_loss * 5 + fc3_loss + fc4_loss # + #conv1_loss + conv2_loss
-total_test_loss = fc1_loss * 100 + fc2_loss + fc3_loss + fc4_loss
+total_train_loss = fc1_loss * 20 + conv1_loss + conv2_loss # + #conv1_loss + conv2_loss
+total_test_loss = fc1_loss * 100 + conv1_loss + conv2_loss
 
 with tf.name_scope('summary'):
     tf.scalar_summary('fc1_loss', fc1_loss)
-    tf.scalar_summary('fc2_loss', fc2_loss)
-    tf.scalar_summary('fc3_loss', fc3_loss)
-    tf.scalar_summary('fc4_loss', fc4_loss)
+    tf.scalar_summary('fc2_loss', conv1_loss)
+    tf.scalar_summary('fc3_loss', conv2_loss)
+    tf.scalar_summary('total_loss', total_train_loss)
+    # tf.scalar_summary('fc4_loss', fc4_loss)
+    '''
     tf.scalar_summary('fc1_weight_loss', fc1_weight_loss)
     tf.scalar_summary('fc2_weight_loss', fc2_weight_loss)
     tf.scalar_summary('fc3_weight_loss', fc3_weight_loss)
@@ -153,23 +150,23 @@ with tf.name_scope('summary'):
     tf.scalar_summary('fc2_reg', fc2_reg)
     #tf.scalar_summary('conv1_loss', conv1_loss)
     #tf.scalar_summary('conv2_loss', conv2_loss)
-    tf.scalar_summary('total_loss', total_train_loss)
+
     tf.histogram_summary('fc1_weight_hist', W_fc1)
     tf.histogram_summary('fc2_weight_hist', W_fc2)
     tf.histogram_summary('fc3_weight_hist', W_fc3)
-    tf.histogram_summary('fc4_weight_hist', W_fc4)
+    tf.histogram_summary('fc4_weight_hist', W_fc4)'''
 
 e_learning_rate = tf.placeholder(tf.float32, shape=[])
 e_step = tf.train.GradientDescentOptimizer(e_learning_rate).minimize(total_train_loss,
-                                                                     var_list=[fc1_var, fc2_var, fc3_var, transform_var],
+                                                                     var_list=[fc1_var, conv1_var, transform_var],
                                                                      name='E_optim')
 m_learning_rate = tf.placeholder(tf.float32, shape=[])
 m_step = tf.train.GradientDescentOptimizer(m_learning_rate).minimize(total_train_loss,
-                                           var_list=[W_fc1, b_fc1, W_fc2, b_fc2, W_fc3, b_fc3, W_fc4, b_fc4],
+                                           var_list=[W_fc1, b_fc1, W_conv1, b_conv1, W_conv2, b_conv2],
                                            name='M_optim')
 
 e_step_test = tf.train.GradientDescentOptimizer(e_learning_rate).minimize(total_test_loss,
-                                                                          var_list=[fc1_var, fc2_var, fc3_var, transform_var, y_weight_var],
+                                                                          var_list=[fc1_var, conv1_var, transform_var, y_weight_var],
                                                                           name='E_optim_test')
 summary_op = tf.merge_all_summaries()
 train_writer = tf.train.SummaryWriter('log/generative', sess.graph)
@@ -177,9 +174,8 @@ train_writer = tf.train.SummaryWriter('log/generative', sess.graph)
 y_vis = tf.placeholder(tf.float32, shape=[1, 12])
 with tf.name_scope("visualization"):
     vis_fc1_relu = tf.nn.relu(tf.matmul(y_vis, W_fc1) + b_fc1)
-    vis_fc2_relu = tf.nn.relu(tf.matmul(vis_fc1_relu, W_fc2) + b_fc2)
-    vis_fc3_relu = tf.nn.relu(tf.matmul(vis_fc2_relu, W_fc3) + b_fc3)
-    image_out = tf.reshape(tf.nn.relu(tf.matmul(vis_fc3_relu, W_fc4, name='fc2') + b_fc4, name='fc2_relu'), [28, 28, 1])
+    vis_conv1_relu = tf.nn.relu(tf.nn.conv2d(unpool(tf.reshape(vis_fc1_relu, (1, 7, 7, 32))), W_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
+    image_out = tf.nn.sigmoid(tf.nn.conv2d(unpool(vis_conv1_relu), W_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
 
 sess.run(tf.initialize_all_variables())
 
@@ -189,17 +185,17 @@ if os.path.isfile('model.ckpt'):
     print("Loading previous network")
 
 e_lr_init = 600
-m_lr = 0.1
+m_lr = 5
 test_lr_init = 800
-e_step_size = 20
+e_step_size = 30
 m_step_size = 10000
 test_step_size = 100
 
 
 def reinitialize():
     sess.run(fc1_var.initializer)
-    sess.run(fc2_var.initializer)
-    sess.run(fc3_var.initializer)
+    sess.run(conv1_var.initializer)
+    # sess.run(conv2_var.initializer)
     # sess.run(conv1_var.initializer)
     sess.run(y_weight_var.initializer)
 
@@ -249,7 +245,7 @@ def visualize():
         for j in range(transform_dim):
             input_label[10 + j] = random.random() * (transform_var_ub[j] - transform_var_lb[j]) + transform_var_lb[j]
         vis_result = sess.run(image_out, feed_dict={y_vis: [input_label]})
-        plt.imshow(vis_result[:, :, 0], cmap=plt.get_cmap('Greys'))
+        plt.imshow(vis_result[0, :, :, 0], cmap=plt.get_cmap('Greys'))
     plt.draw()
     plt.savefig('vis/image' + str(vis_index) + '.png')
     vis_index += 1
@@ -279,9 +275,20 @@ for m_iter in range(m_step_size):
     use_label = True
     reinitialize()
     e_lr = e_lr_init
+    res = []
     for e_iter in range(0, e_step_size):
-        sess.run(e_step, feed_dict={x: batch[0], y_ref: batch[1], train_phase: [use_label]*batch_size, e_learning_rate: e_lr})
-        e_lr *= 0.9
+        loss = sess.run([e_step, total_train_loss], feed_dict={x: batch[0], y_ref: batch[1], train_phase: [use_label]*batch_size, e_learning_rate: e_lr})[1]
+        if e_iter > 15:
+            e_lr *= 0.8
+        res.append(loss)
+        print(e_iter)
+    plt.ioff()
+    plt.plot(res)
+    plt.yscale('log')
+    plt.show()
+    for i in range(24):
+        plt.imshow(sess.run(conv1_var)[0, :, :, i], cmap=plt.get_cmap('Greys'))
+        plt.show()
         # print(sess.run(fc1_var)[0, 0:10])
     transform_value = sess.run(transform_var)
     for b in range(batch_size):
@@ -302,7 +309,7 @@ for m_iter in range(m_step_size):
     train_writer.add_summary(summary_str, m_iter)
     print("Iteration M: " + str(m_iter) + " with loss " + str(loss_result))
     train_writer.flush()
-    if m_iter % 100 == 0:
+    if m_iter % 60 == 0:
         m_lr *= 0.95
 
     if m_iter % 20 == 0 and e_step_size < 30:
@@ -310,7 +317,7 @@ for m_iter in range(m_step_size):
     if m_iter % 50 == 0 and m_iter != 0:
         save_path = saver.save(sess, "model.ckpt")
     if m_iter % 20 == 0:
-        visualize_all()
+        visualize()
 
 
 
