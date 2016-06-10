3dcf069571156478ce980f311b6ae5a0d7d166fd
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-06-10 11:40:54 +0800

    Search unseen images

--- analyze/image_search.py
+++ analyze/image_search.py
@@ -1,12 +1,15 @@
 __author__ = 'shengjia'
 
-from config import settings
-import numpy as np
 
+import numpy as np
+sys.path.insert(0, '..')
+from find_maxes.loaders import load_imagenet_mean, load_labels, caffe
 
 class ImageSearcher:
-    def __init__(self):
-        self.activation = np.load(settings['activation_root'] + 'fc6.npy')
+    def __init__(self, net_prototxt, net_weights, settings):
+        self.settings = settings
+        self.layer = 'conv5'
+        self.activation = np.load(settings['activation_root'] + self.layer + '.npy')[:80000]
         self.num_image = self.activation.shape[0]
 
         # Read in path for all the images
@@ -18,13 +21,36 @@ class ImageSearcher:
                 break
             self.image_list.append(path.strip())
 
+        imagenet_mean = load_imagenet_mean()
+        self.net = caffe.Classifier(net_prototxt, net_weights,
+                                    mean=imagenet_mean,
+                                    channel_swap=(2,1,0),
+                                    raw_scale=255,
+                                    image_dims=(256, 256))
+
     def query(self, query, num_hits=10):
         diff_matrix = self.activation - np.tile(self.activation[query, :], (self.num_image, 1))
         order = np.argsort(np.sum(abs(diff_matrix), 1))
         result = []
         for i in range(0, num_hits):
-            result.append(settings['image_root'] + self.image_list[order[i]])
+            result.append(self.settings['image_root'] + self.image_list[order[i]])
         return result
 
     def get_path(self, index):
-        return settings['image_root'] + self.image_list[index]
+        return self.settings['image_root'] + self.image_list[index]
+
+    def query_image(self, image_path, num_hits=10):
+        im = caffe.io.load_image(image_path)
+        self.net.predict([im], oversample=False)   # Just take center crop
+        layer_shape = self.net.blobs[self.layer].data.shape
+        if len(layer_shape) == 4:
+            result_array = np.amax(self.net.blobs[self.layer].data, (0, 2, 3))
+        else:
+            result_array = np.amax(self.net.blobs[self.layer].data, 0)
+
+        diff_matrix = self.activation - np.tile(result_array, (self.num_image, 1))
+        order = np.argsort(np.sum(abs(diff_matrix), 1))
+        result = []
+        for i in range(0, num_hits):
+            result.append(self.settings['image_root'] + self.image_list[order[i]])
+        return result
