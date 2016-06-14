bea3dd4637953231e828e84cce4ddfd88aba4bb8
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-06-14 16:23:07 +0800

    Fix a bug

--- analyze/image_search.py
+++ analyze/image_search.py
@@ -37,7 +37,7 @@ class ImageSearcher:
             index = np.argsort(np.sum(abs(diff_matrix), 1))[:num_hits]
         else:
             dist, index = self.ball_tree.query(query, k=num_hits)
-            print(dist, index)
+            index = index[0]
         result = []
         for i in range(0, num_hits):
             result.append(self.settings['image_root'] + self.image_list[index[i]])
